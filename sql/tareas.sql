a7ef330f6d550cf156534ed28e8f7e0f6704c1a2
winnershock <winnersolarte749@gmail.com>
2026-07-30 13:27:59 -0500

    Proyecto_tareas

---
+++ sql/tareas.sql
@@ -0,0 +1,14 @@
+create database if not exists todo_db;
+use todo_db;
+
+create table if not exists tasks (
+    id int auto_increment primary key,
+    title varchar(255) not null,
+    description text, 
+    is_completed tinyint(1) default 0 = pendiente, 1 = completa
+    author varchar(100) not null, --Quien creo la tarea (ej: 'Andres', 'Profesor')
+    created_at timestamp default current_timestamp,
+    update_at timestamp default current_timestamp on update current_timestamp
+) ENGINE=InnoDB;
+
+select * from tasks;
